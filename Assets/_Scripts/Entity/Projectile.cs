f4d106d6a4eb72881d744521ad1a96e0f9dbca3b
Stephen Ho <stephen.ho.232@gmail.com>
2025-09-13 22:21:26 +0700

    Adding shooting effect

--- Assets/_Scripts/Entity/Projectile.cs
+++ Assets/_Scripts/Entity/Projectile.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer m_spriteRenderer;
     [SerializeField] protected float m_speed;
     [SerializeField] protected float m_range;
     [SerializeField] protected DamageHandler m_damageHandler;
+    [SerializeField] private ParticleSystem m_hitParticle;
     
+    private bool m_isAlive;
     protected float m_travelledDistance;
     protected Vector2 m_startPos;
 
@@ -24,12 +28,16 @@ public class Projectile : MonoBehaviour
 
     public void Spawn()
     {
+        m_isAlive = true;
         m_travelledDistance = 0;
         m_startPos = transform.position;
+        m_hitParticle.gameObject.SetActive(true);
+        m_spriteRenderer.enabled = true;
     }
     
     protected virtual void UpdateMovement()
     {
+        if (!m_isAlive) return;
         transform.position += transform.up * (m_speed * Time.deltaTime);
         m_travelledDistance = Vector2.Distance(m_startPos, transform.position);
         if (m_travelledDistance >= m_range)
@@ -45,6 +53,19 @@ public class Projectile : MonoBehaviour
 
     protected virtual void DestroyBullet()
     {
+        StartCoroutine(OnDestroyBullet());
+    }
+
+    private IEnumerator OnDestroyBullet()
+    {
+        m_isAlive = false;
+        m_spriteRenderer.enabled = false;
+        
+        if (m_hitParticle != null)
+        {
+            m_hitParticle.Play();
+            yield return new WaitUntil(()=> !m_hitParticle.IsAlive());
+        }
         this.gameObject.SetActive(false);
     }
 }
